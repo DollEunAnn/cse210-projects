cac7063808b7b5937df57d333238cf9a36c64f44
DollEunAnn <euniceannhdollete@gmail.com>
2024-06-20 09:40:30 +0800

    Adding stubs to the Dev 2 - Journal Project

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Runtime.CompilerServices;
+
+public class Journal 
+{
+    public List<Entry> _entries;
+
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveToFile(string file)
+    {
+
+    }
+
+    public void LoadToFile(string file)
+    {
+
+    }
+
+
+
+}
