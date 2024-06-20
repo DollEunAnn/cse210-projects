9d7c65759b401e4b4e12d776a12de78844b8d9bc
DollEunAnn <euniceannhdollete@gmail.com>
2024-06-20 22:01:24 +0800

    adjust newline and added new messages

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -18,15 +18,15 @@ class Program
             Console.WriteLine("3. Load");
             Console.WriteLine("4. Save");
             Console.WriteLine("5. Quit");
-            Console.WriteLine("What would you like to do?");
+            Console.Write("What would you like to do? ");
 
             _userInput = Int32.Parse(Console.ReadLine());
 
             switch (_userInput)
             {
                 case 1:
-                    // Write    
-
+                    // Write
+                    Console.WriteLine();
                     string _prompt = _randomPrompt.GetRandomPrompt();
                     Console.WriteLine($"{_prompt}");
 
@@ -39,37 +39,45 @@ class Program
                     _newEntry._promptText = _prompt;
 
                     theJournal.AddEntry(_newEntry);
+                    Console.WriteLine();
 
                     break;
                 case 2:
                     // Display
-
+                    Console.WriteLine();
                     theJournal.DisplayAll();
 
                     break;
                 case 3:
                     // Load
+                    Console.WriteLine();
                     Console.WriteLine("What is the file name?");
                     file = Console.ReadLine();
+                    Console.WriteLine();
 
                     theJournal.LoadToFile(file);
 
                     break;
                 case 4:
                     // Save
-
+                    Console.WriteLine();
                     Console.WriteLine("What is the file name?");
                     file = Console.ReadLine();
 
                     theJournal.SaveToFile(file);
+                    Console.WriteLine();
 
                     break;
                 case 5:
                     // Quit
+                    Console.WriteLine();
+                    Console.WriteLine("Thank you for using the Journal Program! See you again tomorrow!");
                     Environment.Exit(0);
                     break;
                 default:
+                    Console.WriteLine();
                     Console.WriteLine("Invalid input! Please enter a number between 1 and 5.");
+                    Console.WriteLine();
                     break;
             }
 
