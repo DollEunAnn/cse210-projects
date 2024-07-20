0f68f1f5584a5eeb2fd2dd80d72ecfd3ef7c23bb
DollEunAnn <euniceannhdollete@gmail.com>
2024-07-20 19:26:19 +0800

    Update statement for weather forecast and screenshot

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -40,7 +40,7 @@ class Program
 
         // 3 - Outdoor Gathering
         Address a3 = new Address("J. Escriva 15 St.", "Ortigas Business Center", "Pasig City", "Philippines");
-        OutdoorGathering outdoorGathering = new OutdoorGathering("Boys Scout", "Camping for Boys Scount of the Philippines", "October 12, 2024", "8:00 AM", a3, "Sunny Day - 24C");
+        OutdoorGathering outdoorGathering = new OutdoorGathering("Boys Scout", "Camping for Boys Scount of the Philippines", "October 12, 2024", "8:00 AM", a3, "The weather forecast for Pasig on October 12, 2024, predicts a high of 30°C and a low of 26°C. There is a high chance of thunderstorms throughout the day, with periods of moderate rain expected as well. Humidity levels will be high, around 80-90%, making it feel warmer than the actual temperature. Winds will be moderate, coming from the west-southwest at speeds of 25-30 km/h.");
         string t3 = outdoorGathering.GetType().ToString();
         Console.WriteLine("Standard Details:");
         Console.WriteLine(outdoorGathering.DisplayEventStandardDetails());
